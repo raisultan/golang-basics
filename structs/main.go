730115839355a49f5642a86791ac259ccbc3f43c
raisultan <ki.xbozz@gmail.com>
2021-03-11 15:58:49 +0700

    pointers explained

--- structs/main.go
+++ structs/main.go
@@ -27,12 +27,14 @@ func (p person) print() {
 // compiler makes a copy of value and then passes it to the function body
 // thus, on things like adjusting a value we must pass not a value but a pointer to that value
 
-// "&" and "*"
+// "&" and "*" operators
 // "&" before variable gives address of the variable in the memory
 // "*" before the address of a variable in the memory gives its value that lies on that address
+// "*type" is not pointer, instead it is a type description that says: "pointer to this type"
 
-func (personPointer *person) updateFirstName(newFirstName string) {
-	(*personPointer).firstName = newFirstName
+func (p *person) updateFirstName(newFirstName string) {
+	// get actual value from address and adjust it
+	(*p).firstName = newFirstName
 }
 
 func main() {
@@ -54,7 +56,18 @@ func main() {
 	greg.contact.email = "gregpeterson@peterson.com"
 	greg.contact.zipCode = 6786
 
-	gregPointer := &greg
-	gregPointer.updateFirstName("Gregory")
+	// call updateFirstName() on value pointer
+	// ideally it must written like this (&greg).updateFirstName("Gregory")
+	// but compiler can differentiate and correctly get pointer or value if type of parameter in the function is pointer type
+	greg.updateFirstName("Gregory")
 	greg.print()
 }
+
+// gotcha moment: with reference types u don't need to pass address, it is itself is an address
+// and the change that made to value that is reference type directly changes the value, with no need of passing address
+
+// reference types is a complex data type that looks like wrapper data + value
+// and the wrapper data has pointer to value and when you assign or get reference type, what you are actually receiving is wrapper that already keeps pointer
+
+// VALUE TYPES: int, float, string, bool, struct - use pointers
+// REFERENCE TYPES: slice, map, channel, pointer, function - do not use pointers
