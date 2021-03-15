31a08c7abaff7898279a63eb1b2ee76b3b50a473
raisultan <ki.xbozz@gmail.com>
2021-03-15 10:45:49 +0700

    interfaces: overloading

--- interfaces/main.go
+++ interfaces/main.go
@@ -1,17 +1,25 @@
 package main
 
-type englishBot struct {
-	text string
-}
+import "fmt"
 
-type spanishBot struct {
-	text string
-}
+type englishBot struct{}
 
-func (eb englishBot) getGreeting() string {
+type spanishBot struct{}
+
+// can omit the parameter name in receiver funcs, if param is not used in func itself
+func (englishBot) getGreeting() string {
 	return "Hello, friend!"
 }
 
-func (sb spanishBot) getGreeting() string {
+func (spanishBot) getGreeting() string {
 	return "Hola!"
+}
+
+func printGreeting(eb englishBot) {
+	fmt.Println(eb.getGreeting())
+}
+
+// golang does not support overloading, os u can not have two funcs with the same name
+func printGreeting(sb spanishBot) {
+	fmt.Println(sb.getGreeting())
 }
